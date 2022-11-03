8d3cd9a8e4900c90a56397ca416b2d541d9d01cf
Faiz Ahmed <4faizahmed@gmail.com>
2022-11-03 14:16:35 +0300

    Making a blog post and using redux thunk to fetch it's data

--- blog/src/action/index.js
+++ blog/src/action/index.js
@@ -1,10 +1,22 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from 'lodash'
 
+export const fetchPostsAndUsers = () => async (dispatch,getState) =>{
+  await  dispatch(fetchPosts())
+    console.log(getState().posts)
+    const userIds = _.uniq(_.map(getState().posts,'userId'))
 
-export const fetchPosts = () => {
- return async dispatch =>{
-     const response=await jsonPlaceholder.get('./posts')
-     dispatch({  type:'FETCH_POSTS', payload: response})
- }
-
+    userIds.forEach(id=>dispatch(fetchUser(id)))
 }
+
+export const fetchPosts = () =>  async dispatch =>{
+     const response=await jsonPlaceholder.get('./posts')
+     dispatch({  type:'FETCH_POSTS', payload: response.data})
+ };
+
+
+export  const fetchUser = (id) => async dispatch =>{
+    const response =await jsonPlaceholder.get(`/users/${id}`)
+    dispatch ({type:'FETCH_USER',payload:response.data})
+}
+
